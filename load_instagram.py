b2e10d61fd374dac8d6e88f83b36627a30171926
Maxim Marashan <mmarashan@gmail.com>
2021-09-05 15:49:48 +0400

    add login comented code

--- load_instagram.py
+++ load_instagram.py
@@ -1,10 +1,18 @@
 import instaloader as instl
 
-username = "greenpeaceru"
+username = ""
 
 L = instl.Instaloader(download_geotags=False,
                                  download_comments=False,
                                  save_metadata=True)
+
+# login = ""
+# password = ""
+# try:
+#     loader.login(login, password)
+# except TwoFactorAuthRequiredException as e:
+#     code = input("Input your digit code without space:")
+#     loader.two_factor_login(code)
 
 profiles = {L.check_profile_id(username)}
 L.download_profiles(profiles, profile_pic=False, fast_update=False, raise_errors=True)
